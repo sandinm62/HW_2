5d146d4fddcdb401201f85d662bcd973a18f5c6e
sandinm62 <82060084+sandinm62@users.noreply.github.com>
2022-08-06 12:10:28 +0300

    Добавил третью задачу

--- HW_2_2/Program.cs
+++ HW_2_2/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace HW_2_2
 {
+    //Сандин М.С.
+    //Написать метод подсчета количества цифр числа.
     internal class Program
     {
+        static int counter( int number_input)
+        {
+            int i = 0; // переменная счётчик
+            while (number_input != 0)
+            {
+                i++;
+                number_input = number_input / 10;
+                
+            }
+            return i;
+        }
         static void Main(string[] args)
         {
+            
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The number of digits of the number is {counter(number)}" );
+            Console.ReadKey();
+        
         }
+
+
     }
 }
---
+++ HW_2_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_3
+{
+
+    // Сандин М.С.
+    // С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+    internal class Program
+    {
+        static bool parity(int input_number)
+        {
+            return input_number % 2 == 0 ? true : false;
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int number;
+            int counter = 0;
+            
+            do
+            {
+                Console.Write("Enter the number: ");
+                number = int.Parse(Console.ReadLine());
+                if (number == 0)
+                {
+                    break;
+                } else if ( number < 0)
+                {
+                    continue;
+                }
+
+                if (parity(number) == true)
+                counter++;
+                
+                
+            } while (number != 0);
+
+            Console.WriteLine($"The number of even numbers is {counter}");
+            Console.ReadKey();
+    
+        }
+    }
+}
