f7f669a381b557e63bbbd42574ae149a116a3a7d
sandinm62 <82060084+sandinm62@users.noreply.github.com>
2022-08-06 10:10:10 +0300

    1

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace HW_2
 {
+    //Сандин М.С.
+    //Написать метод, возвращающий минимальное из трёх чисел.
     internal class Program
     {
         static void Main(string[] args)
         {
+            int a, b, c, max;
+            a = 1;
+            b = 2;
+            c = 3;
+
+            if ( a > b && a > c ) 
+            {
+                max = a;
+
+            } else if ( b > c && b > a)
+            {
+                max = b;
+            } else
+            {
+                max = c;
+            }
+
+
         }
     }
 }
